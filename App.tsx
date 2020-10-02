f782dc85fe69bc996bf6e6b3f4b2abfcbf8947b7
nagara.ei <nagara.ei.f@copdev.onmicrosoft.com>
2020-10-02 13:49:20 +0900

    ボトムナビゲータを追加

--- App.tsx
+++ App.tsx
@@ -2,12 +2,36 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//コンポーネント
+import RouteRelatedBottomNavigation from './src/components/RouteRelatedBottomNavigation';
+
+// Route関連
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// 共通スタイル読み込み
+import './App.css';
+
+export const Feed = () => <div>welcome Feed</div>;
+export const Graph = () => <div>Graph</div>;
+export const Columns = () => <div>Columns</div>;
+export const Settings = () => <div>Settings</div>;
+export const NotFound = () => <div>NotFound</div>;
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Feed} />
+          <Route exact path="/graph" component={Graph} />
+          <Route exact path="/columns" component={Columns} />
+          <Route exact path="/settings" component={Settings} />
+          <Route component={NotFound} />
+        </Switch>
+        <RouteRelatedBottomNavigation />
+      </BrowserRouter>
+    </div>
   );
 }
 
