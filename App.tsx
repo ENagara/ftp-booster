844fa979bb8ac30292670325e3a0276f47e6c37e
nagara.ei <nagara.ei.f@copdev.onmicrosoft.com>
2020-10-16 17:00:57 +0900

    UIコンポーネントにreact-native-paperを使用する #8

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import BottomNavigation from './src/components/BottomNavigation';
+import Colors from './src/configs/Colors';
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: Colors.tint,
+  },
+};
 
 export default function App() {
   return (
-    <BottomNavigation></BottomNavigation>
+    <PaperProvider theme={theme}>
+      <BottomNavigation></BottomNavigation>
+    </PaperProvider>
   );
 }
-
